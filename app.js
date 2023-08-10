5fb16b7e6abd5150187feb8b594c80afcf96b499
YangZouy <3075879316@qq.com>
2023-08-10 10:15:41 +0800

    init

---
+++ app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var coolRouter = require('./routes/users/cool');
+
+var app = express();
+
+/* 设置 Mongoose 连接 */
+
+// 导入Mongoose模块
+const mongoose = require("mongoose");
+
+// 默认连接本地数据库 
+const mongoDB = "mongodb://127.0.0.1/library"
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// 让mongoose使用全局的Promise库
+mongoose.Promise = global.Promise;
+// 取得默认连接
+const db = mongoose.connection;
+// 将连接与错误事件绑定（以获得连接错误的提示）
+db.on("error", console.error.bind(console, "MongoDB 连接错误："));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 路径将作为导入路由的前缀，
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('./users/cool',coolRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
